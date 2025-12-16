6d4cf9da14f932ab890529d7a84297a6af0879ec
Rafael Carvalho <raffacarvalho2000@gmail.com>
2025-12-16 12:05:44 +0000

    installed mongo db anc created routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
-import type { Metadata } from "next";
 import { Auth0Provider } from "@auth0/nextjs-auth0/client";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Auth0 Next.js App",
-  description: "Next.js app with Auth0 authentication",
-};
 
 export default function RootLayout({
   children,
